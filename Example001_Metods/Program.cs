8c2c710437f45afffdcdeb9b8c26236ca77507ab
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-06 14:22:37 +0500

    exemple 001 metods

---
+++ Example001_Metods/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вид 1
+void Metod1(){
+  Console.WriteLine("Автор: Евгений Костромин");
+}
+Metod1();
+
+// Вид 2
+void Metod2(string msg){
+  Console.WriteLine(msg);
+}
+Metod2("Текст сообщения 1");
+
+void Metod21(string msg, int count)
+{
+  for (int i = 0; i < count; i++)
+  {
+    Console.WriteLine(msg);
+  }
+}
+Metod21("Текст сообщения", 4);
