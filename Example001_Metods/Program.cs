ff7769bba04333904cbd17743f1eb11644503060
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-06 14:57:13 +0500

    Добавил виды методов 3 и 4

--- Example001_Metods/Program.cs
+++ Example001_Metods/Program.cs
@@ -1,10 +1,11 @@
-﻿// Вид 1
+﻿// Вид 1 ничего не принимают, ничего не возвращают
+
 void Metod1(){
   Console.WriteLine("Автор: Евгений Костромин");
 }
 Metod1();
 
-// Вид 2
+// Вид 2 что-то принимают, ничего не возвращают
 void Metod2(string msg){
   Console.WriteLine(msg);
 }
@@ -18,3 +19,29 @@ void Metod21(string msg, int count)
   }
 }
 Metod21("Текст сообщения", 4);
+
+
+// Вид 3 ничего не принимают, что-то возвращают
+
+int Metod3(){
+
+  return DateTime.Now.Year;
+}
+int year = Metod3();
+Console.WriteLine(year);
+
+
+// Вид 4 что - то принимают, что - то возвращают
+
+string Metod4(int count, string text){
+
+  int i = 0;
+  string result = "";
+  while(i < count){
+    result = result + text;
+    i++;
+  } 
+  return result;
+}
+string res = Metod4(10, "z");
+Console.WriteLine(res);
