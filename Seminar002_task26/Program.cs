dd913ae210544bc5c3ebc5cc1481c8be3e0bfe28
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-10 17:09:34 +0500

    задача 26

---
+++ Seminar002_task26/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает 
+//на вход число N и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.Write("Ввдите число: ");
+int s = int.Parse(Console.ReadLine()!);
+Console.Write($"Количество цифр = {CountNumber(s)}");
+
+int CountNumber(int n){
+  int count = 0;
+  if (n < 0)
+  {
+    n *= (-1); // Умножил на -1 если число отрицательное
+  }
+  while (n > 0)
+  {
+    n /= 10;
+    count++;
+  }
+  return count;
+}
