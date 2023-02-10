fb25431ec8ccc97cab2d8d632a05ef6d0fdd1efc
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-10 16:29:03 +0500

    задача 24

---
+++ Seminar001/Program.cs
@@ -0,0 +1,19 @@
+﻿//24 Напишите программу, которая принимает на вход число A
+// и выдаёт сумму чисел от 1 до A
+// a = 5
+// sum = 1 + 2 + 3 + 4 + 5
+
+Console.Clear();
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine()!);
+
+int GetSum(int limit){
+  int sum = 0;
+  for (int i = 1; i <= limit; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+Console.WriteLine($"сумма числа {a} рана {GetSum(a)}");
