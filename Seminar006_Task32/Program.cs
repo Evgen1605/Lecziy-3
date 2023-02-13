803ac82948f84dfae6d481acd1280807aa7a6293
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-02-13 14:27:16 +0500

    Задач 32

---
+++ Seminar006_Task32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return res;
+}
+
+int[] array = GetArray(6, -10, 10);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] InversArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] *= -1; 
+  }
+  return array;
+}
+
+Console.WriteLine($"[{String.Join(", ", InversArray(array))}]");
